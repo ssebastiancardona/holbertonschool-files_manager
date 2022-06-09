8af1649e50c86369353b9b843cb9d8e6c6f4dfbf
ssebastiancardona <ssebastiancardona@gmail.com>
2022-06-09 16:18:48 -0500

    fix route

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -2,20 +2,14 @@ import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
 class AppController {
-  static getStatus(req, res) {
-    const status = {
-      redis: redisClient.isAlive(),
-      db: dbClient.isAlive(),
-    };
-    res.status(200).send(status);
+  static getStatus(request, response) {
+    const resObj = { redis: redisClient.isAlive(), db: dbClient.isAlive() };
+    return response.status(200).send(resObj);
   }
 
-  static async getStats(req, res) {
-    const stats = {
-      users: await dbClient.nbUsers(),
-      files: await dbClient.nbFiles(),
-    };
-    res.status(200).send(stats);
+  static async getStats(request, response) {
+    const resObj = { users: await dbClient.nbUsers(), files: await dbClient.nbFiles() };
+    return response.status(200).send(resObj);
   }
 }
 
--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 import express from 'express';
-import controllerRouting from './routes/index';
+import indexRouter from './routes/index';
 
-const exPort = process.env.PORT || 5000;
 const app = express();
 
+const port = process.env.PORT || 5000;
+
 app.use(express.json());
+app.use('/', indexRouter);
+app.listen(port, console.log(`Server running on port ${port}`));
 
-controllerRouting(app);
-
-app.listen(exPort, () => {
-  console.log(`Server running on port ${exPort}`);
-});
+export default app;
